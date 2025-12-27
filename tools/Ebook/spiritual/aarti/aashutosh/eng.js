88d6d8259df9bdac630751a59426ea5d71b5b63e
JKMPDF <mpurohit2414@gmail.com>
2025-12-27 22:59:21 +0530

    n

---
+++ tools/Ebook/spiritual/aarti/aashutosh/eng.js
@@ -0,0 +1,34 @@
+var englishContent = `    shri Ashutosh Maharaj Ji Ki Aarati
+
+He Ashutosh, Jay Ashutosh
+Deenabandhu, He Ashu Pita, 
+Arati Karen Teri Bhagavan
+Nit Charanan Nan Men Sheesh Jhukaeँ, 
+Karen Tumhara Vandan।
+Dukhaharta, Sukhakarta Tum Hi, 
+Bhakton Ke Bhav-bhanjan Karunakar,
+Karunamay Svami, 
+Arpit Tumako Tan-man।
+Mat-pita Tum Bandhu Sakha Ho, 
+Shvason Ke Ho Spandan
+Man Mandir Men Vas Tumhara, 
+Ho Tera Aradhan।
+Ghat-ghat Vasi Ho Avinashi, 
+He Devan Ke Devan
+Tumhari Moorat Nit Nayan Men,
+Shvas-shvas Karen Poojan।
+Dhyan Dhare Jo Bhakt Tumhara, 
+Kare Tumhara Chintan Seva-sumiran Ratn Tumhare,
+Pave Nit-nit Darshan।
+Ho Vishvas Sada Dridha़ Aisa, 
+Vichalit N Ho Ye Man Lagan Men Teri Magan Rahen Ham, 
+Bhakti Karen Ajeevan ।
+Aisi Bhakti Bana Do Guruvar, 
+Toote N Ye Bandhan
+Janm-janm Ham Paeँ Tumako, 
+As Karo Ye Pooran ।
+Jnan Prakash Ke Sahaj Puँj Ho,
+Tam Viheen Kar Do Man
+Jnanapradayak, Shakti Pradayak Shat-shat Tumako Vandan।
+
+`;
---
+++ tools/Ebook/spiritual/chalisha/durga/eng.js
@@ -0,0 +1,127 @@
+var englishContent = `   shri Durga Chalisha
+
+॥ Chaupai ॥
+Namo Namo Durge Sukh Karani ।
+Namo Namo Ambe Duahkh Harani ॥
+
+Nirakar Hai Jyoti Tumhari ।
+Tihooँ Lok Phaili Ujiyari ॥
+
+Shashi Lalat Mukh Mahavishala ।
+Netr Lal Bhrikuti Vikarala ॥
+
+Roop Matu Ko Adhik Suhave ।
+Darash Karat Jan Ati Sukh Pave ॥
+
+Tum Sansar Shakti Lay Keena ।
+Palan Hetu Ann Dhan Deena ॥
+
+Annapoorna Hui Jag Pala ।
+Tum Hi Adi Sundari Bala ॥
+
+Pralayakal Sab Nashan Hari ।
+Tum Gauri Shivashankar Pyari ॥
+
+Shiv Yogi Tumhare Gun Gaven ।
+Brahma Vishnu Tumhen Nit Dhyaven ॥
+
+Roop Sarasvati Ko Tum Dhara ।
+De Subuddhi Rishi-munin Ubara ॥
+
+Dhara Roop Narasinh Ko Amba ।
+Pragat Bhaeen Phaड़kar Khamba ॥
+
+Raksha Kar Prahlad Bachayo ।
+Hiranyaksh Ko Svarg Pathayo ॥
+
+Lakshmi Roop Dharo Jag Maheen ।
+Shri Narayan Ang Samaheen ॥
+
+Ksheerasindhu Men Karat Vilasa ।
+Dayasindhu Deejai Man Asa ॥
+
+Hingalaj Men Tumheen Bhavani ।
+Mahima Amit N Jat Bakhani ॥
+
+Matangi Aru Dhoomavati Mata ।
+Bhuvaneshvari Bagala Sukh Data ॥
+
+Shri Bhairav Tara Jag Tarini ।
+Chhinn Bhal Bhav Duahkh Nivarini ॥
+
+Kehari Vahan Soh Bhavani ।
+Langur Veer Chalat Agavani ॥
+
+Kar Men Khappar-khadg Virajai ।
+Jako Dekh Kal Dar Bhaje ॥
+
+Sohai Astr Aur Trishoola ।
+Jate Uthat Shatru Hiy Shoola ॥
+
+Nagar Koti Men Tumheen Virajat ।
+Tihunlok Men Danka Bajat ॥
+
+Shumbh Nishumbh Danav Tum Mare ।
+Raktabeej Shankhan Sanhare ॥
+
+Mahishasur Nrip Ati Abhimani ।
+Jehi Agh Bhar Mahi Akulani ॥
+
+Roop Karal Kalika Dhara ।
+Sen Sahit Tum Tihi Sanhara ॥
+
+Pari Gaढ़ Santan Par Jab-jab ।
+Bhai Sahay Matu Tum Tab Tab ॥
+
+Amarapuri Aru Basav Loka ।
+Tab Mahima Sab Rahen Ashoka ॥
+
+Jvala Men Hai Jyoti Tumhari ।
+Tumhen Sada Poojen Nar-nari ॥
+
+Prem Bhakti Se Jo Yash Gavai ।
+Duahkh Daridr Nikat Nahin Aven ॥
+
+Dhyave Tumhen Jo Nar Man Lai ।
+Janm-maran Takau Chhuti Jai ॥
+
+Jogi Sur Muni Kahat Pukari ।
+Yog N Ho Bin Shakti Tumhari ॥
+
+Shankar Acharaj Tap Keeno ।
+Kam Aru Krodh Jeeti Sab Leeno ॥
+
+Nishidin Dhyan Dharo Shankar Ko ।
+Kahu Kal Nahin Sumiro Tumako ॥
+
+Shakti Roop Ko Maram N Payo ।
+Shakti Gai Tab Man Pachhitayo ॥
+
+Sharanagat Hui Keerti Bakhani ।
+Jay Jay Jay Jagadamb Bhavani ॥
+
+Bhai Prasann Adi Jagadamba ।
+Dai Shakti Nahin Keen Vilamba ॥
+
+Moko Matu Kasht Ati Ghero ।
+Tum Bin Kaun Harai Duahkh Mero ॥
+
+Asha Trishna Nipat Satave ।
+Moh Madadik Sab Vinashavai ॥
+
+Shatru Nash Keejai Maharani ।
+Sumiraun Ikachit Tumhen Bhavani ॥
+
+Karo Kripa He Matu Dayala ।
+Riddhi-siddhi De Karahu Nihala ॥
+
+Jab Lagi Jiyaun Daya Phal Paoon ।
+Tumharo Yash Main Sada Sunaoon ॥
+
+Durga Chaleesa Jo Nit Gavai ।
+Sab Sukh Bhog Paramapad Pavai ॥
+
+Deveedas Sharan Nij Jani ।
+Karahu Kripa Jagadamb Bhavani ॥ 
+
+`;
