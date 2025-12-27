5566a2b4109aae61445f3ed16559cdcdf881e194
JKMPDF <mpurohit2414@gmail.com>
2025-12-27 21:30:49 +0530

    new

---
+++ tools/Ebook/spiritual/chalisha/hanuman/eng.js
@@ -0,0 +1,138 @@
+var englishContent = `    Shree Hanuman Chalisa
+
+    Doha
+Shri Guru Charan Sarooja-raj Nija manu Mukura Sudhaari
+Baranau Rahubhara Bimala Yasha Jo Dayaka Phala Chari
+Budhee-Heen Thanu Jannikay Sumirow Pavana Kumara
+Bala-Budhee Vidya Dehoo Mohee Harahu Kalesha Vikaara
+
+    Chopai
+
+     1.  Jai Hanuman gyan gun sagar ||
+         Jai Kapis tihun lok ujagar ||
+
+     2.  Ram doot atulit bal dhama |
+         Anjaani-putra Pavan sut nama ||
+
+     3.  Mahabir Bikram Bajrangi |
+         Kumati nivar sumati Ke sangi ||
+
+     4.  Kanchan varan viraj subesa |
+         Kanan Kundal Kunchit Kesha ||
+
+     5.  Hath Vajra Aur Dhuvaje Viraje |
+         Kaandhe moonj janehu sajai ||
+
+     6.  Sankar suvan kesri Nandan |
+         Tej prataap maha jag vandan ||
+
+     7.  Vidyavaan guni ati chatur |
+         Ram kaj karibe ko aatur ||
+
+     8.  Prabu charitra sunibe-ko rasiya |
+         Ram Lakhan Sita man Basiya ||
+
+     9.  Sukshma roop dhari Siyahi dikhava |
+	 Vikat roop dhari lank jarava ||
+
+     10. Bhima roop dhari asur sanghare |
+	 Ramachandra ke kaj sanvare ||
+
+     11. Laye Sanjivan Lakhan Jiyaye |
+	 Shri Raghuvir Harashi ur laye ||
+
+     12. Raghupati Kinhi bahut badai |
+	 Tum mam priye Bharat-hi-sam bhai ||
+
+     13. Sahas badan tumharo jash gaave |
+	 Asa-kahi Shripati kanth lagaave ||
+
+     14. Sankadhik Brahmaadi Muneesa |
+         Narad-Sarad sahit Aheesa ||
+
+     15. Jam Kuber Digpaal Jahan te |
+	 Kavi kovid kahi sake kahan te ||
+
+     16. Tum upkar Sugreevahin keenha |
+	 Ram milaye rajpad deenha ||
+
+     17. Tumharo mantra Vibheeshan maana |
+	 Lankeshwar Bhaye Sub jag jana ||
+
+     18. Jug sahastra jojan par Bhanu |
+         Leelyo tahi madhur phal janu ||
+
+     19. Prabhu mudrika meli mukh mahee |
+         Jaladhi langhi gaye achraj nahee ||
+
+     20. Durgaam kaj jagath ke jete |
+	 Sugam anugraha tumhre tete ||
+
+     21. Ram dwaare tum rakhvare |
+	 Hoat na agya binu paisare ||
+
+     22. Sub sukh lahae tumhari sar na |
+	 Tum rakshak kahu ko dar naa ||
+
+     23. Aapan tej samharo aapai |
+	 Teenhon lok hank te kanpai ||
+
+     24. Bhoot pisaach Nikat nahin aavai |
+	 Mahavir jab naam sunavae ||
+
+     25. Nase rog harae sab peera |
+	 Japat nirantar Hanumant beera ||
+
+     26. Sankat se Hanuman chudavae |
+  	 Man Karam Vachan dyan jo lavai ||
+
+     27. Sab par Ram tapasvee raja |
+	 Tin ke kaj sakal Tum saja ||
+
+     28. Aur manorath jo koi lavai |
+	 Sohi amit jeevan phal pavai ||
+
+     29. Charon Yug partap tumhara |
+	 Hai persidh jagat ujiyara ||
+
+     30. Sadhu Sant ke tum Rakhware |
+         Asur nikandan Ram dulhare ||
+
+     31. Ashta-sidhi nav nidhi ke dhata |
+ 	 As-var deen Janki mata ||
+
+     32. Ram rasayan tumhare pasa |
+	 Sada raho Raghupati ke dasa ||
+
+     33. Tumhare bhajan Ram ko pavai |
+	 Janam-janam ke dukh bisraavai ||
+
+     34. Anth-kaal Raghuvir pur jayee |
+	 Jahan janam Hari-Bakht Kahayee ||
+
+     35. Aur Devta Chit na dharehi |
+	 Hanumanth se hi sarve sukh karehi ||
+
+     36. Sankat kate-mite sab peera |
+	 Jo sumirai Hanumat Balbeera ||
+
+     37. Jai Jai Jai Hanuman Gosahin |
+	 Kripa Karahu Gurudev ki nyahin ||
+
+     38. Jo sat bar path kare kohi |
+	 Chutehi bandhi maha sukh hohi ||
+
+     39. Jo yah padhe Hanuman Chalisa |
+	 Hoye siddhi sakhi Gaureesa ||
+
+     40. Tulsidas sada hari chera |
+	 Keejai Nath Hridaye mein dera ||
+
+     Doha
+	 Pavan Tanay Sankat Harana | 
+	 Mangala Murati Roop ||
+	 Ram Lakhan Sita Sahita |
+       	 Hriday Basahu Soor Bhoop ||
+
+
+  `;
---
+++ tools/Ebook/spiritual/chalisha/hanuman/hin.js
@@ -0,0 +1,139 @@
+var englishContent = `    श्री हनुमान चालीसा
+
+    Doha
+श्रीगुरु चरन सरोज रज, निज मनु मुकुरु सुधारि ।
+बरनऊं रघुबर बिमल जसु, जो दायकु फल चारि ।। 
+बुद्धिहीन तनु जानिके, सुमिरौं पवन-कुमार ।
+बल बुद्धि बिद्या देहु मोहिं, हरहु कलेस बिकार ।।
+
+    दोहा :
+
+     1.  जय हनुमान ज्ञान गुन सागर ।
+         जय कपीस तिहुं लोक उजागर ।।
+
+     2.  रामदूत अतुलित बल धामा ।
+ 	 अंजनि-पुत्र पवनसुत नामा ।।
+
+     3.  महाबीर बिक्रम बजरंगी ।
+         कुमति निवार सुमति के संगी ।।
+
+     4.  कंचन बरन बिराज सुबेसा ।
+         कानन कुंडल कुंचित केसा ।।
+
+     5.  हाथ बज्र औ ध्वजा बिराजै ।
+         कांधे मूंज जनेऊ साजै ।।
+
+     6.  संकर सुवन केसरीनंदन ।
+         तेज प्रताप महा जग बन्दन ।।
+
+     7.  विद्यावान गुनी अति चातुर ।
+	 राम काज करिबे को आतुर ।।
+
+     8.  प्रभु चरित्र सुनिबे को रसिया । 
+	 राम लखन सीता मन बसिया ।।
+
+     9.  सूक्ष्म रूप धरि सियहिं दिखावा ।
+ 	 बिकट रूप धरि लंक जरावा ।।
+
+     10. Bhima roop dhari asur sanghare |
+	 Ramachandra ke kaj sanvare ||
+
+     11. Laye Sanjivan Lakhan Jiyaye |
+	 Shri Raghuvir Harashi ur laye ||
+
+     12. Raghupati Kinhi bahut badai |
+	 Tum mam priye Bharat-hi-sam bhai ||
+
+     13. Sahas badan tumharo jash gaave |
+	 Asa-kahi Shripati kanth lagaave ||
+
+     14. Sankadhik Brahmaadi Muneesa |
+         Narad-Sarad sahit Aheesa ||
+
+     15. Jam Kuber Digpaal Jahan te |
+	 Kavi kovid kahi sake kahan te ||
+
+     16. Tum upkar Sugreevahin keenha |
+	 Ram milaye rajpad deenha ||
+
+     17. Tumharo mantra Vibheeshan maana |
+	 Lankeshwar Bhaye Sub jag jana ||
+
+     18. Jug sahastra jojan par Bhanu |
+         Leelyo tahi madhur phal janu ||
+
+     19. Prabhu mudrika meli mukh mahee |
+         Jaladhi langhi gaye achraj nahee ||
+
+     20. Durgaam kaj jagath ke jete |
+	 Sugam anugraha tumhre tete ||
+
+     21. Ram dwaare tum rakhvare |
+	 Hoat na agya binu paisare ||
+
+     22. Sub sukh lahae tumhari sar na |
+	 Tum rakshak kahu ko dar naa ||
+
+     23. Aapan tej samharo aapai |
+	 Teenhon lok hank te kanpai ||
+
+     24. Bhoot pisaach Nikat nahin aavai |
+	 Mahavir jab naam sunavae ||
+
+     25. Nase rog harae sab peera |
+	 Japat nirantar Hanumant beera ||
+
+     26. Sankat se Hanuman chudavae |
+  	 Man Karam Vachan dyan jo lavai ||
+
+     27. Sab par Ram tapasvee raja |
+	 Tin ke kaj sakal Tum saja ||
+
+     28. Aur manorath jo koi lavai |
+	 Sohi amit jeevan phal pavai ||
+
+     29. Charon Yug partap tumhara |
+	 Hai persidh jagat ujiyara ||
+
+     30. Sadhu Sant ke tum Rakhware |
+         Asur nikandan Ram dulhare ||
+
+     31. Ashta-sidhi nav nidhi ke dhata |
+ 	 As-var deen Janki mata ||
+
+     32. Ram rasayan tumhare pasa |
+	 Sada raho Raghupati ke dasa ||
+
+     33. Tumhare bhajan Ram ko pavai |
+	 Janam-janam ke dukh bisraavai ||
+
+     34. Anth-kaal Raghuvir pur jayee |
+	 Jahan janam Hari-Bakht Kahayee ||
+
+     35. Aur Devta Chit na dharehi |
+	 Hanumanth se hi sarve sukh karehi ||
+
+     36. Sankat kate-mite sab peera |
+	 Jo sumirai Hanumat Balbeera ||
+
+     37. Jai Jai Jai Hanuman Gosahin |
+	 Kripa Karahu Gurudev ki nyahin ||
+
+     38. Jo sat bar path kare kohi |
+	 Chutehi bandhi maha sukh hohi ||
+
+     39. Jo yah padhe Hanuman Chalisa |
+	 Hoye siddhi sakhi Gaureesa ||
+
+     40. Tulsidas sada hari chera |
+	 Keejai Nath Hridaye mein dera ||
+
+     दोहा :
+
+	 Pavan Tanay Sankat Harana | 
+	 Mangala Murati Roop ||
+	 Ram Lakhan Sita Sahita |
+       	 Hriday Basahu Soor Bhoop ||
+
+
+  `;
